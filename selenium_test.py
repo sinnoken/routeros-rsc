7a00c231fbc2f0150f4c1ac4e981bfabdb237e09
hsieh c. yicheng <sinnoken@live.com>
2025-03-31 01:11:15 +0800

    Update selenium_test.py

--- selenium_test.py
+++ selenium_test.py
@@ -1,13 +1,24 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
+# 從環境變數讀取 Chrome 路徑
+chrome_path = os.getenv('CHROME_PATH')  # 這是從 GitHub Actions 中傳遞過來的
+chromedriver_path = os.getenv('CHROMEDRIVER_PATH')  # 這是從 GitHub Actions 中傳遞過來的
+print(f"Chrome Path: {chrome_path}")
+print(f"ChromeDriver Path: {chromedriver_path}")
+
 # 設定 Chrome 瀏覽器
 options = webdriver.ChromeOptions()
 options.add_argument("--headless")  # 無頭模式
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
 
-driver = webdriver.Chrome(options=options)
+# 指定 Chrome 瀏覽器的路徑
+options.binary_location = chrome_path
+
+# 指定 ChromeDriver 路徑
+driver = webdriver.Chrome(executable_path=chromedriver_path, options=options)
+
 
 url = "https://internet-measurement.com/#ips"
 driver.get(url)
@@ -17,3 +28,5 @@ element = driver.find_element(By.CSS_SELECTOR, "#ips > pre:nth-child(1)")
 print(element.text)  # 取出元素內的文本
 
 driver.quit()
+
+
